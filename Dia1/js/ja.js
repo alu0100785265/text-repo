e9edb65398f10552182958f5ac191b76e0e548ba
alu0100785265 <alu0100785265@ull.edu.es>
2016-10-19 16:05:54 +0100

    :bowtie:

---
+++ Dia1/js/ja.js
@@ -0,0 +1,9 @@
+function mensaje() {
+			swal({
+			title: 'Título',
+			text: 'Mensaje de texto',
+			html: '<p>Mensaje de texto con <strong>formato</strong>.</p>',
+			type: 'success',
+			timer: 3000,
+			});
+			}

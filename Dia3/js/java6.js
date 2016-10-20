0bbe171ff402adeded145851250ec73b948a5f71
alu0100785265 <alu0100785265@ull.edu.es>
2016-10-20 19:47:56 +0100

    empezando

--- Dia3/js/java6.js
+++ Dia3/js/java6.js
@@ -7,7 +7,9 @@ loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
 });
 */
 var PSV = PhotoSphereViewer({
-  panorama: 'https://cdn.rawgit.com/mistic100/Photo-Sphere-Viewer/master/example/Bryce-Canyon-National-Park-Mark-Doliner.jpg',
+  //panorama: 'https://cdn.rawgit.com/mistic100/Photo-Sphere-Viewer/master/example/Bryce-Canyon-National-Park-Mark-Doliner.jpg',
+  panorama: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfgP0G6bs6dLoSeRB_0TX0sV7Rui8e0IhehJAo_o0IOJ1tM1Bm',
+
   container: 'photosphere',
   //caption: 'Hola amigos míos',
   //navbar: false,

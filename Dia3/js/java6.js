7e5e9bacf0c27511dc1d69987031b5963dd9b0f2
alu0100785265 <alu0100785265@ull.edu.es>
2016-10-20 19:50:12 +0100

    empezando

--- Dia3/js/java6.js
+++ Dia3/js/java6.js
@@ -8,7 +8,7 @@ loading_img: 'https://media.giphy.com/media/TtZqlvHid7BjW/giphy.gif',
 */
 var PSV = PhotoSphereViewer({
   //panorama: 'https://cdn.rawgit.com/mistic100/Photo-Sphere-Viewer/master/example/Bryce-Canyon-National-Park-Mark-Doliner.jpg',
-  panorama: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfgP0G6bs6dLoSeRB_0TX0sV7Rui8e0IhehJAo_o0IOJ1tM1Bm',
+  panorama: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTEVqt0FxzmMfpJ2K0KHV_NtfVqbaffBxpou0y0C7P_EXuM-hp9',
 
   container: 'photosphere',
   //caption: 'Hola amigos míos',
